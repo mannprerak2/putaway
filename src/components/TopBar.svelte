a9ed60f34d5adb3e8f21131e6660890e794eec09
asubedy <frexpe@pm.me>
2020-10-03 09:50:14 +0545

    installled seveltejs-fontawesome package and changed search icon of newtab.html

--- src/components/TopBar.svelte
+++ src/components/TopBar.svelte
@@ -1,13 +1,14 @@
 <script>
     import { searchText } from './../stores/searchTextStore.js'
-
+    import Fa from "sveltejs-fontawesome"
+    import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch'
     export let darkTheme;
     export let toggleTheme;
 </script>
 <style>
     input {
         padding: 6px;
-        padding-left: 20px;
+        padding-left: 50px;
         border: 1px dashed gray;
         border-radius: 20px;
         margin-top: 8px;
@@ -17,11 +18,23 @@
         background-color: var(--bg);
         color: var(--txt);
     }
+    .search-logo{
+        position: absolute;
+        left: 1rem;
+        opacity: 0.7;
+        top:1.5rem;
+        
+    }
 </style>
 
 <main>
     <div class="flex-row-container">
-        <input type="text" placeholder="🔍 Search" bind:value={$searchText}/>
+        <input type="text" placeholder="Search" bind:value={$searchText}/>
+        <div class = "search-logo">
+            <Fa 
+            icon={faSearch}
+            size="3x" ></Fa>
+        </div>
         <div style="flex-grow:1;"/>
         {#if darkTheme}
         <div on:click={toggleTheme} class="pointer" style="font-size: 2em;">☽</div>
